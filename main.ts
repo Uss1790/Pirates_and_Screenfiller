e0e1beeae75ae0d739e869d3e766c2ed5dc1394a
Uss1790 <114951501+Uss1790@users.noreply.github.com>
2022-11-24 09:03:18 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,90 @@
+let counter = 0
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    music.setTempo(45)
+    music.playTone(294, music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Eighth))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Eighth))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.playTone(466, music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Eighth))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Eighth))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Eighth))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.playTone(466, music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Eighth))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Eighth))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Eighth))
+    music.playTone(466, music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(622, music.beat(BeatFraction.Quarter))
+    music.playTone(622, music.beat(BeatFraction.Quarter))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Eighth))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.playTone(587, music.beat(BeatFraction.Eighth))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Eighth))
+    music.playTone(466, music.beat(BeatFraction.Eighth))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Eighth))
+    music.playTone(349, music.beat(BeatFraction.Eighth))
+    music.playTone(392, music.beat(BeatFraction.Half))
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    let point: number[];
+    let x: number;
+    let y: number;
+    
+    while (true) {
+        point : number[] = []
+        counter += 1
+        x = Math.trunc(Math.random() * 5)
+        y = Math.trunc(Math.random() * 5)
+        if (_py.py_array_index(point, x + y / 10) < 0) {
+            basic.pause(100)
+            led.plot(x, y)
+            point.push(x + y / 10)
+        }
+        
+        if (point.length == 25) {
+            basic.showNumber(counter)
+            break
+        }
+        
+    }
+})
+basic.forever(function on_forever() {
+    
+})
